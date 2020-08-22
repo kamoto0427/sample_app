9c054ac9f634fcbf917d4b2b8e1461afcce7f288
kamoto0427 <takahiro.08039@gmail.com>
2020-08-22 11:38:42 +0900

    クレジット/ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items
-  resources :credit_cards
+  resources :credit_cards do
+    collection do
+      post 'show', to: 'creditCard#show'
+      post 'create', to: 'creditCard#create'
+      post 'delete', to: 'creditCard#delete'
+    end
+  end
   resources :users
 end
